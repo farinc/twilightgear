d5706d75b6f7ade87c3a9ae6fa49d75f1b7771f3
Forrest Ramirez <forrest.ram64@gmail.com>
2023-08-13 15:26:59 -0700

    Remove the burning trait, as I have also come to dislike it in principle. Like, in general, the ignite modifier does the same thing, only more controlled. Also it is closer to the original twilight forest tinkers integration, which I prefer.

--- src/main/java/com/farinc/twilightgear/init/InitItem.java
+++ src/main/java/com/farinc/twilightgear/init/InitItem.java
@@ -1,12 +1,10 @@
 package com.farinc.twilightgear.init;
 
-import com.farinc.twilightgear.item.AshItem;
 import com.farinc.twilightgear.item.CarminiteDust;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.RegistryObject;
 
 public final class InitItem {
 
-    public static final RegistryObject<Item> ITEM_ASH = Init.ITEMS.register("ash", AshItem::new);
     public static final RegistryObject<Item> ITEM_CARMINITE_DUST = Init.ITEMS.register("carminite_dust", CarminiteDust::new);
 }
--- src/main/java/com/farinc/twilightgear/init/InitTraits.java
+++ src/main/java/com/farinc/twilightgear/init/InitTraits.java
@@ -1,13 +1,11 @@
 package com.farinc.twilightgear.init;
 
-import com.farinc.twilightgear.traits.BurningTrait;
 import com.farinc.twilightgear.traits.PhotosynthesisTrait;
 import net.silentchaos512.gear.api.GearApi;
 
 public final class InitTraits {
 
     public static void register() {
-        GearApi.registerTraitSerializer(BurningTrait.SERIALIZER);
         GearApi.registerTraitSerializer(PhotosynthesisTrait.SERIALIZER);
     }
 }
