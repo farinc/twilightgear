f6bc4726c3a6dc8c6283b2e7d8b42c83597a68ce
Forrest Ramirez <forrest.ram64@gmail.com>
2023-08-15 16:37:18 -0700

    Removed the carminite dust thing, not sure what I was even thinking at the time but I dont think its is required.

--- src/main/java/com/farinc/twilightgear/item/CarminiteDust.java
+++
@@ -1,9 +0,0 @@
-package com.farinc.twilightgear.item;
-
-import net.minecraft.world.item.Item;
-
-public class CarminiteDust extends Item {
-    public CarminiteDust() {
-        super(new Properties());
-    }
-}
