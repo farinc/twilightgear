8cb0e7e6732a1a79f30df253ce11a73bfe753b47
Forrest Ramirez <forrest.ram64@gmail.com>
2023-09-06 00:37:19 -0700

    Added trollsteinn as a material. Features its own trait that enables the user to check if the light level is at or below 7 , a feature that the trollsteinn block features in Twilight Forest.

--- src/main/java/com/farinc/twilightgear/init/InitTraits.java
+++ src/main/java/com/farinc/twilightgear/init/InitTraits.java
@@ -1,11 +1,14 @@
 package com.farinc.twilightgear.init;
 
 import com.farinc.twilightgear.traits.PhotosynthesisTrait;
+import com.farinc.twilightgear.traits.SensitiveTrait;
+
 import net.silentchaos512.gear.api.GearApi;
 
 public final class InitTraits {
 
     public static void register() {
         GearApi.registerTraitSerializer(PhotosynthesisTrait.SERIALIZER);
+        GearApi.registerTraitSerializer(SensitiveTrait.SERIALIZER);
     }
 }
---
+++ src/main/java/com/farinc/twilightgear/traits/SensitiveTrait.java
@@ -0,0 +1,51 @@
+package com.farinc.twilightgear.traits;
+
+import com.farinc.twilightgear.TwilightGear;
+import com.mojang.math.Vector3f;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.DustParticleOptions;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.FakePlayer;
+import net.silentchaos512.gear.gear.trait.SimpleTrait;
+
+public class SensitiveTrait extends SimpleTrait {
+
+    public static final Serializer<SensitiveTrait> SERIALIZER = new Serializer<>(
+            TwilightGear.getId("sensitive"), SensitiveTrait::new);
+
+    public SensitiveTrait(ResourceLocation id) {
+        super(id, SERIALIZER);
+    }
+
+    private final static int LIGHT_THRESHOLD = 7; // should mirror the one in twilightforest.block.TrollsteinnBlock.
+                                                  // Avoids asm hacks...
+
+    @Override
+    public InteractionResult onItemUse(UseOnContext context, int traitLevel) {
+        Player player = context.getPlayer();
+        if (player != null && !player.isSpectator()
+                && !(player instanceof FakePlayer)) {
+            Level level = player.level;
+            BlockPos position = context.getClickedPos().above();
+            if (level.isClientSide) {
+                int brightness = level.getMaxLocalRawBrightness(position);
+                if (brightness <= LIGHT_THRESHOLD) {
+                    for (int i = 0; i < 5; i++) {
+                        double x = position.getX() + level.random.nextFloat();
+                        double y = position.getY() + level.random.nextFloat() / 4;
+                        double z = position.getZ() + level.random.nextFloat();
+                        level.addParticle(new DustParticleOptions(
+                                new Vector3f(0.5F, 0.0F, 0.5F), 1.0F),
+                                x, y, z, 0.25D, -1.0D, 0.5D);
+                    }
+                }
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+}
